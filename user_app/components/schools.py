e1a403fe4a7a9803505c6932758ae92caf41f60d
Nishan-sys <ictnishan@gmail.com>
2025-10-29 19:05:32 +0530

    component update

--- user_app/components/schools.py
+++ user_app/components/schools.py
@@ -30,9 +30,7 @@ class SchoolsView(UnicornView):
     def load_schools(self):
         """Schools load කරන්න"""
         if self.selected_division_id:
-            self.schools = School.objects.filter(
-                division_id=self.selected_division_id
-            ).select_related('division').order_by('name')
+            self.schools = School.objects.filter(division_id=self.selected_division_id).select_related('division').order_by('name')
         else:
             self.schools = []
     
