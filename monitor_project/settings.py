681d47b1f17a1fb11f0177af468ebccdd4b38c23
Nishan-sys <ictnishan@gmail.com>
2026-03-29 14:36:50 +0530

    trying ipv4

--- monitor_project/settings.py
+++ monitor_project/settings.py
@@ -70,10 +70,13 @@ WSGI_APPLICATION = 'monitor_project.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default=os.getenv('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=not DEBUG
+        conn_max_age=60,
+        ssl_require=True
     )
 }
+
+
+
 
 # Localdatabase configuration for development
 # DATABASES = {
